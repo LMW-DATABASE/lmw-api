2e617c7416d114b8d8bc4f040be89e37873a5763
VitorCampos15 <vitorcampos152.0@gmail.com>
2026-04-19 11:32:05 -0300

    dockerfile

--- src/lmw/settings/production.py
+++ src/lmw/settings/production.py
@@ -6,8 +6,8 @@ import dj_database_url
 
 
 DEBUG = False
-
 ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'api.lmwdatabase.com').split(',')
+#ALLOWED_HOSTS = ['*']#teste
 
 DATABASE_URL = os.getenv('DATABASE_URL')
 
@@ -29,10 +29,11 @@ else:
 
 CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS', 'https://lmwdatabase.com').split(',')
 
-CSRF_TRUSTED_ORIGINS = [f"https://{host}" for host in ALLOWED_HOSTS]
-SECURE_SSL_REDIRECT = True
+CSRF_TRUSTED_ORIGINS = [f"http://{host}" for host in ALLOWED_HOSTS]# removendo httpS
+SECURE_SSL_REDIRECT = False # Desativando para teste
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 SECURE_HSTS_SECONDS = 31536000 # 1 ano
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_PRELOAD = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
